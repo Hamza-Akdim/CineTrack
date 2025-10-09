48e9fef00ac7ead9c1d0c1ff226d95eaaf1ae542
AKDIM <akdimhamza879@gmail.com>
2025-10-10 00:39:26 +0200

    firebase connection

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,22 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { environment } from './environments/environment';
 import { routes } from './app.routes';
 
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+
+    // Initialize Firebase App using environment config
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+
+    // Firebase services
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+  ],
 };
---
+++ src/app/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDHS1hHFCXvLpQ6rVIz2pG56Fs9GN0Be7Y",
+    authDomain: "cinetrack-48eda.firebaseapp.com",
+    projectId: "cinetrack-48eda",
+    storageBucket: "cinetrack-48eda.firebasestorage.app",
+    messagingSenderId: "285338591435",
+    appId: "1:285338591435:web:72c4035d055dd151e0fb4e"
+  }
+};
